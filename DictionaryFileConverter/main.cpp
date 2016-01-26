64c7f43b4a12cc006cd68ceba20ba80b9770491a
MicahElizabeth <micahejenkins@yahoo.com>
2016-01-25 20:36:51 -0800

    Initial Commit

---
+++ DictionaryFileConverter/main.cpp
@@ -0,0 +1,2 @@
+//This app will take in a file with a list of words, read it into a tree, and then print it to a file
+//In a way that would be more efficient to read into a tree. (tree called a trie)
